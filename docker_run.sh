551658be6a9e00f58d3aac6715b39da466f19367
Rupeshsolanki <43853729+Rupeshsolanki@users.noreply.github.com>
2020-11-01 14:07:46 +0530

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker build -t jenkins2 .
 docker run --name=jobs -itd -p 8080:8080 jenkins2
+sleep 40
 docker exec jobs  jenkins-jobs --conf /etc/jenkins_jobs/jenkins_jobs.ini --user=admin --password=admin123 update jobs
 
